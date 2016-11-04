48332d456db2e30559c18d7d117c04c719ce3084
Benjamin Eikel <benjamin@eikel.org>
2016-11-04 16:38:08 +0100

    Remove unneeded files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libutil):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libutil" command="/usr/bin/libutil"
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-liblibutil 0.2.0 libutil (>> 0.2.0-0), libutil (<< 0.2.0-99)
